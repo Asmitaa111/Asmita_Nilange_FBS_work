4d4cb4eece49d016074aa1f59c9a0e90b8885271
Asmitaa111 <asmitanilange@gmail.com>
2026-08-15 01:49:52 +0530

    Add files via upload

---
+++ Assignment 2/GreatestNumber.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, greatest;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a > b)
+    {
+        if(a > c)
+        {
+            greatest = a;
+        }
+        else
+        {
+            greatest = c;
+        }
+    }
+    else
+    {
+        if(b > c)
+        {
+            greatest = b;
+        }
+        else
+        {
+            greatest = c;
+        }
+    }
+
+    printf("Greatest = %d", greatest);
+
+    return 0;
+}
